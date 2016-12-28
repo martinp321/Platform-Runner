ae73703edc84c68063c4e36015c7fabd56353f1f
Martin Pogorzelski <martinp321@gmail.com>
2016-12-28 10:08:59 -0500

    Initial Commit

---
+++ Assets/Scripts/PlatformManager1.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformManager1 : SkylineManager
+{
+    public Vector3 minGap, maxGap;
+    public float minY, maxY;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    private void Recycle()
+    {
+        Vector3 scale = new Vector3(
+            Random.Range(minSize.x, maxSize.x),
+            Random.Range(minSize.y, maxSize.y),
+            Random.Range(minSize.z, maxSize.z));
+
+        Vector3 position = nextPosition;
+        position.x += scale.x * 0.5f;
+        position.y += scale.y * 0.5f;
+
+        Transform o = objectQueue.Dequeue();
+        o.localScale = scale;
+        o.localPosition = position;
+        objectQueue.Enqueue(o);
+
+        nextPosition += new Vector3(
+            Random.Range(minGap.x, maxGap.x) + scale.x,
+            Random.Range(minGap.y, maxGap.y),
+            Random.Range(minGap.z, maxGap.z));
+
+        if (nextPosition.y < minY)
+        {
+            nextPosition.y = minY + maxGap.y;
+        }
+        else if (nextPosition.y > maxY)
+        {
+            nextPosition.y = maxY - maxGap.y;
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Runner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Runner : MonoBehaviour
+{
+    public static float distanceTraveled;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(5f * Time.deltaTime, 0f, 0f);
+        distanceTraveled = transform.localPosition.x;
+        transform.Translate(Input.acceleration.x, 0f, -Input.acceleration.y);
+    }
+}
